1988ef6e2f620ea74a22334a701a89f3ec559bc3
kholstinevich <kholstinin.da@gmail.com>
2024-12-11 18:25:25 +0500

    fix: tests in 2 homeowrk

--- test/project.js
+++ test/project.js
@@ -4,8 +4,9 @@ const { exec } = require('node:child_process');
 const actual = {
   name: "infra-homework",
   version: "1.0.0",
+  author: "kholstinin.da@gmail.com",
   description: "homework tasks for frontend infra course",
-  dependencies: ["eslint: ^9.16.0", "yaml: 2.6.1"]
+  dependencies: ["eslint: 9.16.0", "yaml: 2.6.1"]
 };
 
 exec('yarn project', (_, stdout) => {
